cad48c779ced729301bd6a6d4bfca0ef1c57920a
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2021-07-23 22:56:40 -0500

    Update xBound.h

--- src/Core/x/xBound.h
+++ src/Core/x/xBound.h
@@ -25,8 +25,18 @@ struct xBound
 #define XBOUND_TYPE_CYL 3 // Cylinder (xBound::cyl)
 #define XBOUND_TYPE_OBB 4 // Oriented bounding box (xBound::box + xBound::mat)
 
+void xBoundUpdate(xBound* b);
+void xBoundGetBox(xBox& box, const xBound& bound);
+void xBoundGetCylinder(xCylinder& o, const xBound& bound);
+void xBoundGetSphere(xSphere& o, const xBound& bound);
 void xBoundSphereHitsOBB(const xSphere* s, const xBox* b, const xMat4x3* m, xCollis* coll);
+void xBoundHitsBound(const xBound* a, const xBound* b, xCollis* c);
 void xRayHitsBound(const xRay3* r, const xBound* b, xCollis* c);
 void xSphereHitsBound(const xSphere* o, const xBound* b, xCollis* c);
+bool xBoundHitsBound(const xBound& a, const xBound& b);
+bool xRayHitsBound(const xRay3& r, const xBound& b);
+bool xSphereHitsBound(const xVec3& c, float32 r, const xBound& b);
+bool xOBBHitsBound(const xBox& box, const xMat4x3& mat, const xBound& b);
+void xBoundGetCenter(const xBound* b, xVec3* center);
 
 #endif
