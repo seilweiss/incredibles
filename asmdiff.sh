9b04848d44d92f977bd28d13dca9afb6561438c8
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2022-01-17 22:54:08 -0500

    repair asmdiff.sh

--- asmdiff.sh
+++ asmdiff.sh
@@ -2,5 +2,5 @@
 
 OBJDUMP="$DEVKITPPC/bin/powerpc-eabi-objdump -D -bbinary -EB -mpowerpc -M gekko --adjust-vma=0x80003200"
 !(test -f baserom.dump) && $OBJDUMP baserom.dol > baserom.dump
-!(test -f main.dump) && $OBJDUMP main.dol > main.dump
+!(test -f main.dump) && $OBJDUMP build/in.usa/main.dol > main.dump
 diff -u --color=always baserom.dump main.dump | head -n 2000
